46a1860aa653dc01a3e9bc7a9602ba8d130257d5
Cooper Saye <coopersaye@gmail.com>
2022-08-21 16:30:13 -0700

    Construct path page content.

--- pages/[path].tsx
+++ pages/[path].tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link';
 import styles from '../styles/pages/Path.module.scss';
 
 export default function Path() {
   return (
     <div className={styles.container}>
+      <div className={styles.center}>
+        <h1>Link not found</h1>
+        <Link href="/">
+          <a>return to homepage</a>
+        </Link>
+      </div>
     </div>
   );
 }
