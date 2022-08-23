cc79dc46fc88a8921b57a9679aaaab09fe90e9c1
Cooper Saye <coopersaye@gmail.com>
2022-08-22 21:58:30 -0700

    Create _document page.

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800;900&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
